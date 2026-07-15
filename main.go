e8b8014ef92c93b81bd3bbcb943b4ff729d11d30
Kashif Soofi <kashif.soofi+git@gmail.com>
2026-07-15 22:06:59 +0100

    done

--- main.go
+++ main.go
@@ -7,13 +7,11 @@ import (
 func main() {
 	var n int
 	fmt.Scan(&n)
-	if n%15 == 0 {
-		fmt.Println("FizzBuzz")
-	} else if n%5 == 0 {
-		fmt.Println("Buzz")
-	} else if n%3 == 0 {
-		fmt.Println("Fizz")
-	} else {
-		fmt.Println(n)
+
+	total := 0
+	for i := 0; i <= n; i++ {
+		total += i
 	}
+
+	fmt.Println(total)
 }

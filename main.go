141ec81c7d433280482e1f5d409018577eca8afa
Kashif Soofi <kashif.soofi+git@gmail.com>
2026-07-16 18:09:10 +0100

    done

--- main.go
+++ main.go
@@ -4,25 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
 func main() {
 	r := bufio.NewReader(os.Stdin)
 	line, _ := r.ReadString('\n')
-	parts := strings.Fields(strings.TrimSpace(line))
-	nums := make([]int, 0, len(parts))
-	for _, p := range parts {
-		n, _ := strconv.Atoi(p)
-		nums = append(nums, n)
+	words := strings.Fields(strings.TrimSpace(line))
+	seen := map[string]bool{}
+	// Add each word and print the size of seen.
+	for _, v := range words {
+		seen[v] = true
 	}
-	// Find and print the maximum.
-	largest := nums[0]
-	for i := 0; i < len(nums); i++ {
-		if nums[i] > largest {
-			largest = nums[i]
-		}
-	}
-	fmt.Println(largest)
+	fmt.Println(len(seen))
 }

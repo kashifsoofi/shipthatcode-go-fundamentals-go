20596c0136ac40f9f62198ffe253eb0f52a114df
Kashif Soofi <kashif.soofi+git@gmail.com>
2026-07-16 18:01:56 +0100

    done

--- main.go
+++ main.go
@@ -11,12 +11,18 @@ import (
 func main() {
 	r := bufio.NewReader(os.Stdin)
 	line, _ := r.ReadString('\n')
-	line = strings.TrimRight(line, "\r\n")
-	n, err := strconv.Atoi(line)
-	if err != nil {
-		fmt.Println("bad")
-		return
+	parts := strings.Fields(strings.TrimSpace(line))
+	nums := make([]int, 0, len(parts))
+	for _, p := range parts {
+		n, _ := strconv.Atoi(p)
+		nums = append(nums, n)
 	}
-
-	fmt.Printf("ok %d", n)
+	// Find and print the maximum.
+	largest := nums[0]
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > largest {
+			largest = nums[i]
+		}
+	}
+	fmt.Println(largest)
 }

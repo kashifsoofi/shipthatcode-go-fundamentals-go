97c9dd7fa65a66c36298625fcd038043f3556b46
Kashif Soofi <kashif.soofi+git@gmail.com>
2026-07-16 18:20:18 +0100

    done

--- main.go
+++ main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
 )
 
+type Point struct {
+	X, Y int
+}
+
+func (p Point) EuclideanDistance(p1 Point) int {
+	dx := p1.X - p.X
+	dy := p1.Y - p.Y
+	return (dx * dx) + (dy * dy)
+}
+
 func main() {
-	r := bufio.NewReader(os.Stdin)
-	line, _ := r.ReadString('\n')
-	words := strings.Fields(strings.TrimSpace(line))
-	seen := map[string]bool{}
-	// Add each word and print the size of seen.
-	for _, v := range words {
-		seen[v] = true
-	}
-	fmt.Println(len(seen))
+	var x1, y1, x2, y2 int
+	fmt.Scan(&x1)
+	fmt.Scan(&y1)
+	fmt.Scan(&x2)
+	fmt.Scan(&y2)
+	// Build two Points and print squared distance.
+	p1 := Point{X: x1, Y: y1}
+	p2 := Point{X: x2, Y: y2}
+
+	d := p1.EuclideanDistance(p2)
+	fmt.Println(d)
 }
